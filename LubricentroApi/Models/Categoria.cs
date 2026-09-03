fb244422cedad645a293214296989f519d5706fb
Agre1011 <rojasgabriel157@gmail.com>
2026-09-03 16:24:59 -0300

    Cree la clase categoria

---
+++ LubricentroApi/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LubricentroApi.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [MaxLength(250)]
+        public string? Descripcion { get; set; }
+    }
+}
